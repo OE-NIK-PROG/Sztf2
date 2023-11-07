c9c0c48173fe37d62dba49aa51c79636994141e0
vekonymartin <41823341+vekonymartin@users.noreply.github.com>
2023-11-07 20:10:37 +0100

    Create Hazi_Szuperhos.cs

---
+++ 9.Labor/Hazi_Szuperhos.cs
@@ -0,0 +1,18 @@
+using Hazi.enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hazi.classes
+{
+    internal class Szuperhos
+    {
+        public string Név { get; set; }
+        public bool Mutáns { get; set; }
+        public int Erő { get; set; }
+        public int Gyorsaság { get; set; }
+        public Oldal Oldal { get; set; }
+    }
+}
