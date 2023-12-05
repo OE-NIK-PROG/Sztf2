2482602597412a3fa81560d1211d635f4488b19a
vekonymartin <41823341+vekonymartin@users.noreply.github.com>
2023-12-05 23:01:21 +0100

    Update Termek.cs

--- ZH GYAK/Termek.cs
+++ ZH GYAK/Termek.cs
@@ -1,33 +1,33 @@
-﻿using System;
+using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace minta_zh
+namespace zh_gyak
 {
-	public delegate void ArValtozasDelegate();
+    public delegate void ArValtozasDelegate();
 
     public class Termek
     {
-		private int ar;
+        private int ar;
+        private string nev;
+        
+        public event ArValtozasDelegate ArValtozes;
+        public int Ar { get { return ar; } set { ar = value; ArValtozasFigyelo(); } }
+        public string Nev { get { return nev; } set { nev = value; ArValtozasFigyelo(); } }
 
-		public int Ar
-		{
-			get { return ar; }
-			set { ar = value; ArValtozasFigyelo(); }
-		}
-
-		public event ArValtozasDelegate ArValtozes;
-
-		protected void ArValtozasFigyelo() 
-		{
-			ArValtozes?.Invoke();
-		}
+        public void ArValtozasFigyelo() 
+        {
+            ArValtozes?.Invoke();
+        }
+        
+        public void Felvetel()
+        {
+            Console.WriteLine("Termeket felvetelre kerult!");
+        }
 
         public override string ToString()
         {
-			return $"{Ar}";
+            return $"{Nev} >> {Ar}";
         }
     }
 }
